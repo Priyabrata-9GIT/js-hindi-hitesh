1cded291b8ee3647bbfdd43cb49f48389328ffb3
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-10 12:26:01 +0000

    CHANGING

---
+++ .devcontainer/01_basics/data_convertion.js
@@ -0,0 +1,27 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+let valiNum= Number(score)
+//console.log(typeof valiNum);
+let scorei="33abc"
+//console.log(typeof (scorei));
+let valiNumi= Number(scorei)
+//console.log(typeof valiNumi);
+//console.log(valiNumi);
+//"33"=>number
+//"45cvf"=> NaN(Not a number)
+//null=>0
+//undefined=>NaN
+//True/False=>1/0
+//"madhab"=>NaN
+let bool=0
+let boolnu= Boolean(bool)
+console.log(boolnu)
+//0=>false
+//1=>true
+//""=>false
+//"fifwhf"=>true
+let somenum=84
+let word=String(somenum)
+console.log(word);
+console.log(typeof(word));
