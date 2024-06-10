19f90422c26340570d369ef144f6d3cc4d30dc21
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-10 08:29:51 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"Use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+console.log("priya")
+let name = "priya"
+let age = 18
+let isLogin= false
+// number=> 2 to the pow 53
+//bigint
+//string=""
+//boolean=>true/false
+//NULL=>standalone value
+//undefined
+console.log(typeof null)
+console.log(typeof undefined)
