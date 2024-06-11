1b95cf46fd4015402139e06532504b4eaa3bc903
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-11 12:12:38 +0000

    return type

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -12,3 +12,17 @@ let isLogin= false
 //undefined
 console.log(typeof null)
 console.log(typeof undefined)
+/* Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
