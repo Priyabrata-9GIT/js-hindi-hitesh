9fa7681337acfc69fa5b834d27cff6b60e78a4fb
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-24 18:09:01 +0000

    array

---
+++ .devcontainer/02_basics/array.js
@@ -0,0 +1,61 @@
+// array
+const myArr = [1, 2 ,4 ,5, 6, 7];
+/*console.log(myArr);
+console.log(myArr[0]);
+// in js arr performs shallow copy(same reference point)
+// in js arr can  contain more than one type of variables
+let myarr = [1,4, "hii","ballon" ,8.4]
+console.log(myarr);
+// arr can be declare as this also
+const myarr2 = new Array(3,5,6,7,9)
+console.log(myarr2[2])
+myArr.push(8);
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
+myArr.unshift(12)
+console.log(myArr);
+myArr.shift()
+console.log(myArr);*/
+
+/*
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
+*/
+//slice and splice
+/*
+const arr1 = myArr.slice(1,3)
+console.log("A", myArr);
+console.log(arr1);
+const arr2 = myArr.splice(1,3)
+console.log("B", myArr);
+console.log(arr2);
+*/ 
+
+/*
+const marvel = ["thor","hulk"];
+const dc = ["superman","Batman","flash"];
+marvel.push(dc);
+const arr1=marvel.concat(dc);
+console.log(arr1);
+//console.log(marvel[3][2]);
+const arr3 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+
+const children = arr3.concat(arr2); 
+console.log(children);
+const allhero=[...arr3, ...arr2]
+console.log(allhero)
+let me= [1,2,3,[4,5,6],7,[8,9,[10,11]]]
+let anme= me.flat(1)
+console.log(anme);
+let annme=me.flat(Infinity)
+console.log(annme);
+*/
+console.log(Array.isArray("priyabrata"));
+console.log(Array.from("priyabrata"));
+
