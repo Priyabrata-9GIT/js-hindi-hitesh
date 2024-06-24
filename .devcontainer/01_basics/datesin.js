ebaec66aef16d9e4641b44e8f7a5624d52186485
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-24 05:09:48 +0000

    date and time

---
+++ .devcontainer/01_basics/datesin.js
@@ -0,0 +1,30 @@
+let mydate = new Date;
+/*console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toJSON());
+console.log(mydate.toISOString());*/
+//let creatdate = new Date(2023,0,21)
+//console.log(creatdate.toLocaleDateString());
+/*let creatdate = new Date(2023,0,21,5,6,22)
+console.log(creatdate.toLocaleString());
+let mycrtdate= new Date("2023-04-26")
+let youcrtdate= new Date("01-13-2023")
+console.log(mycrtdate.toLocaleString());
+console.log(youcrtdate.toLocaleString());
+let myTimestamp = Date.now();
+console.log(myTimestamp);
+console.log(mycrtdate.getTime());
+console.log(Date.now());
+console.log(Math.floor(Date.now()/1000));*/
+let newDate= new Date;
+console.log(newDate);
+console.log(newDate.getMonth());//jan as 0
+console.log(newDate.getDay());//sunday as 0
+`${newDate.getDay()} and the time is`
+
+
+
