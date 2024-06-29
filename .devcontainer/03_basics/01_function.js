35d4388d0307c582cb77ea8daff3be689fd7da94
Priyabrata-9GIT <priyabratanath521@gmail.com>
2024-06-29 06:06:09 +0000

    function

---
+++ .devcontainer/03_basics/01_function.js
@@ -0,0 +1,69 @@
+function add(num1,num2){
+    //console.log(num1+num2);
+    return num1+num2;
+}
+const sumu= add(3,4);
+//console.log("result :",sumu);
+
+
+
+function loginUserMessage(username){
+    if(username==undefined){
+        console.log("plz enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage());
+
+/*function loginUserMessage(username){
+    if(!username){
+        console.log("plz enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage());*/
+
+function loginUserMessage(username="sam"){
+    if(username==undefined){
+        console.log("plz enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+//console.log(loginUserMessage());
+//console.log(loginUserMessage("ritik"));
+
+/*function calcartprice( ...num1){
+    return num1;
+}
+console.log(calcartprice(100,200,599,499,300));
+ */
+
+function calcartprice(val1,val2,...num1){
+    return num1;
+}
+//console.log(calcartprice(100,200,599,499,300));
+ 
+const newarr=[23,45,67,12,48]
+function secondvalue(getarr){
+    return getarr[1]
+}
+//console.log(secondvalue(newarr));
+
+const user = {
+    usernmae:"priya",
+    id: 84
+}
+function handleobject(user){
+    console.log(`username is ${user.username} and price is ${user.id}`);
+
+}
+//handleobject(user)
+
+handleobject({
+    username:"josua",
+    id: 844
+}) 
+    
